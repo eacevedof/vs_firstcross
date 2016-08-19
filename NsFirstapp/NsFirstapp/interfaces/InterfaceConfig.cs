cea2c93745ee4930b27270e127b8d985d3db47cb
Eduardo <Eduardo@LAPDES-EDUARDO>
2016-08-19 10:34:26 +0200

    InterfaceConfig 1.0.0

---
+++ NsFirstapp/NsFirstapp/interfaces/InterfaceConfig.cs
@@ -0,0 +1,15 @@
+﻿/**
+* @file:\NsFirstapp\NsFirstapp\interfaces\InterfaceConfig.cs 1.0.0
+**/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NsFirstapp.interfaces
+{
+    interface InterfaceConfig
+    {
+    }//InterfaceConfig
+}//NsFirstapp.interfaces

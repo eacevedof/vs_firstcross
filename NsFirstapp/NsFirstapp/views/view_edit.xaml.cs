629cf095ab0b2fe265fe614e38e5bdd638bd0b4c
Eduardo A. F. (lap_thosiba) <eacevedof@yahoo.es>
2016-08-24 09:41:35 +0200

    backup

---
+++ NsFirstapp/NsFirstapp/views/view_edit.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using NsFirstapp.models;
+using NsFirstapp.components;
+using NsFirstapp.helpers;
+
+namespace NsFirstapp.views
+{
+    public partial class ViewEdit : ContentPage
+    {
+        public ViewEdit()
+        {
+            InitializeComponent();
+        }//ViewEdit
+    }//ViewEdit
+}//NsFirstapp.views

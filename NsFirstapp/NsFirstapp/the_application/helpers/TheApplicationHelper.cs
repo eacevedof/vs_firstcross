95aa04681a7ccc891b80aafdfca3155e83d12f6f
Eduardo A. F. (lap_thosiba) <eacevedof@yahoo.es>
2016-08-26 11:00:16 +0200

    clases app-helper,model,view y config

---
+++ NsFirstapp/NsFirstapp/the_application/helpers/TheApplicationHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NSTheframework.Core;
+namespace NSTheapplication.Components
+{
+    public class TheApplicationHelper: TheFrameworkHelper
+    {
+        public TheApplicationHelper():base()
+        {
+
+        }//TheApplicationHelper()
+    }//TheApplicationHelper
+}//NSTheapplication.Components
---
+++ NsFirstapp/NsFirstapp/the_application/views/TheApplicationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NSTheframework.Core;
+namespace NSTheapplication.Components
+{
+    public class TheApplicationView: TheFrameworkView
+    {
+        public TheApplicationView():base()
+        {
+
+        }//TheApplicationView()
+    }//TheApplicationView
+}//NSTheapplication.Components

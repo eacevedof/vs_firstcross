03d9a896a7b481cd043c37fba750efa5473dd6a5
Eduardo A. F. (lap_thosiba) <eacevedof@yahoo.es>
2016-08-30 10:34:34 +0200

    agrego NS DependencyService

--- NsFirstapp/NsFirstapp/the_framework/core/TheFrameworkModel.cs
+++ NsFirstapp/NsFirstapp/the_framework/core/TheFrameworkModel.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿/*
+ *@file: \NsFirstapp\NsFirstapp\the_framework\core\TheFrameworkModel.cs 1.0.0
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Xamarin.Forms;
 using SQLite.Net;
+
 using NSTheframework.Components;
 
 namespace NSTheframework.Core 
@@ -18,6 +23,16 @@ namespace NSTheframework.Core
 
         }//TheFrameworkModel()
 
+        public void db_connect()
+        {
+            //https://developer.xamarin.com/guides/cross-platform/application_fundamentals/data/part_3_using_sqlite_orm/
+            var oCompConfig = DependencyService.Get<InterfaceConfig>();
+            this.oSQLiteConn = new SQLiteConnection(oCompConfig.get_platform
+                , Path.Combine(oCompConfig.get_db_folder, this.sDbName));
+
+            this.oSQLiteConn.CreateTable<ModelEmpleado>();
+
+        }//db_connect
 
 
         public void Dispose()
